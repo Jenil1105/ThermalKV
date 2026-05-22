83e50192837e0e187684f7bdbffc906df6ce2cd5
Jenil1105 <unselfstudy@gmail.com>
2026-05-22 20:18:26 +0530

    Implement basic store functionality with Set and Get methods

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,14 @@ import (
 func main() {
 	db := store.NewStore()
 
+	db.Set("name:first", "jenil")
+	db.Set("empty", "")
+
 	fmt.Println("ThermalKV starting...")
-	fmt.Println(db.Data)
+	value, exists := db.Get("empty")
+	if exists {
+		fmt.Println("value:", value)
+	} else {
+		fmt.Println("Key not found...")
+	}
 }
--- internal/store/store.go
+++ internal/store/store.go
@@ -9,3 +9,12 @@ func NewStore() *Store {
 		Data: make(map[string]string),
 	}
 }
+
+func (s *Store) Set(key string, value string) {
+	s.Data[key] = value
+}
+
+func (s *Store) Get(key string) (string, bool) {
+	value, exists := s.Data[key]
+	return value, exists
+}

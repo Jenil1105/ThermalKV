d7be1ecf6086083b352f20c448aef1adc76076c2
Jenil1105 <unselfstudy@gmail.com>
2026-06-04 22:33:02 +0530

    Add refrigerator for cooling the keys

---
+++ internal/store/refrigerator.go
@@ -0,0 +1,101 @@
+package store
+
+import (
+	"fmt"
+	"thermalkv/internal/model"
+	"time"
+)
+
+func (s *Store) StartCoolingWorker() {
+	go func() {
+		for {
+			time.Sleep(500 * time.Second)
+
+			var keysToCool []string
+
+			s.Mutex.RLock()
+
+			for key, item := range s.Data {
+				if s.CoolingScore(item) > s.CoolingThreshold {
+					keysToCool = append(keysToCool, key)
+				}
+			}
+
+			s.Mutex.RUnlock()
+
+			for _, key := range keysToCool {
+				err := s.CoolKey(key)
+
+				if err == nil {
+					fmt.Println("Auto cooled:", key)
+				}
+			}
+
+		}
+	}()
+}
+
+func (s *Store) CoolingScore(
+	item model.Item,
+) int64 {
+
+	idleTime := time.Now().Unix() - item.LastAccessUnix
+
+	return item.Size * idleTime
+}
+
+func (s *Store) RunEmergencyCooling() {
+
+	s.Mutex.Lock()
+
+	if s.CoolingInProgress {
+		s.Mutex.Unlock()
+		return
+	}
+
+	s.CoolingInProgress = true
+	s.Mutex.Unlock()
+
+	defer func() {
+		s.Mutex.Lock()
+		s.CoolingInProgress = false
+		s.Mutex.Unlock()
+	}()
+
+	for {
+
+		if s.CurrentMemoryUsage <= s.MaxHotMemory {
+			return
+		}
+
+		var maxKey string
+		var maxScore int64
+
+		s.Mutex.RLock()
+
+		for key, item := range s.Data {
+			score := s.CoolingScore(item)
+
+			if score > maxScore {
+				maxScore = score
+				maxKey = key
+			}
+		}
+
+		s.Mutex.RUnlock()
+
+		if maxKey == "" {
+			return
+		}
+
+		err := s.CoolKey(maxKey)
+
+		if err != nil {
+			return
+		}
+
+		fmt.Println("Emerfency cooled:", maxKey, "Score:", maxScore)
+
+	}
+
+}
